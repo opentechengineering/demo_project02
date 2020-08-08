95b208b648a970f0888821e8966d22f5eae9f835
opentechengineering <69385460+opentechengineering@users.noreply.github.com>
2020-08-08 22:02:54 +0100

    Update Demo002.java
    changes

--- src/demo_project02/Demo002.java
+++ src/demo_project02/Demo002.java
@@ -6,6 +6,7 @@ public class Demo002 {
 	
 			System.out.println("Our first commit from local repo");
 			System.out.println("Our second commit from local repository");
+			System.out.println("Our second commit from local repository 2");
 
 	}
 
