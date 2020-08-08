1208fbdbadb487984de1c2f4cf55400097dd2f44
feti <fkarakoc02@gmail.com>
2020-08-08 21:53:08 +0100

    deneme

--- src/demo_project02/Demo002.java
+++ src/demo_project02/Demo002.java
@@ -3,7 +3,9 @@ package demo_project02;
 public class Demo002 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+			System.out.println("Our first commit from local repo");
+			System.out.println("Our second commit from local repository");
 
 	}
 
